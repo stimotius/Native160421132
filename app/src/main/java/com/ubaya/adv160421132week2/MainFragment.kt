9fe1a4887d560ba24d7450e1cb6500738522c5ac
stimotius <147174427+stimotius@users.noreply.github.com>
2024-03-05 19:00:42 +0700

    init

--- app/src/main/java/com/ubaya/adv160421132week2/MainFragment.kt
+++ app/src/main/java/com/ubaya/adv160421132week2/MainFragment.kt
@@ -1,5 +1,7 @@
 package com.ubaya.adv160421132week2
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.ubaya.adv160421132week2.databinding.FragmentMainBinding
+import kotlin.random.Random
 
 /**
  * A simple [Fragment] subclass.
@@ -32,10 +35,28 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.txtAngka1.text = Random.nextInt(1, 100).toString()
+        binding.txtAngka2.text = Random.nextInt(1, 100).toString()
+        var score = 0
         binding.btnStart.setOnClickListener {
-            val playerName = binding.txtName.text.toString()
-            val action = MainFragmentDirections.actionGameFragment(playerName)
-            Navigation.findNavController(it).navigate(action)
+            val result = (binding.txtAngka1.text.toString().toInt() + binding.txtAngka2.text.toString().toInt())
+            val answer = binding.txtAnswer.text.toString()
+            if (answer.toInt() == result) {
+                val dialog = AlertDialog.Builder(activity)
+                dialog.setTitle("Message")
+                score++
+                dialog.setMessage("Your answer is correct.\nYour score is: $score")
+                dialog.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+                    binding.txtAnswer.text?.clear()
+                    binding.txtAngka1.text = Random.nextInt(1, 100).toString()
+                    binding.txtAngka2.text = Random.nextInt(1, 100).toString()
+                })
+                dialog.create().show()
+            } else {
+                val res = MainFragmentDirections.actionResultFragment(score.toString())
+                Navigation.findNavController(it).navigate(res)
+                score = 0
+            }
         }
     }
 }
